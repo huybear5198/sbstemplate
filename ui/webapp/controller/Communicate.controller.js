caf37d9fd563b3297a3f941249d62a10f54fe0eb
Vu Quoc Huy (RBVH/ETI24) <huy.vuquoc@vn.bosch.com>
2022-09-12 16:39:49 +0700

    sbs-fiori

---
+++ ui/webapp/controller/Communicate.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "./MyController",
+], function (MyController) {
+	"use strict";
+
+	return MyController.extend("com.bosch.sbs.vuu7hc.ui.controller.Communicate", {
+		onInit: function () {
+			
+		},
+
+		onAfterRendering: function () {
+			
+		}
+	});
+});
---
+++ ui/webapp/test/unit/AllTests.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    // "com/bosch/sbs/vuu7hc/ui/test/unit/controller/App.controller",
+    "com/bosch/sbs/vuu7hc/ui/test/unit/controller/MyController",
+    "com/bosch/sbs/vuu7hc/ui/test/unit/controller/BaseController",
+    "com/bosch/sbs/vuu7hc/ui/test/unit/controller/purchaseOrderMaster.controller"
+    // "com/bosch/sbs/vuu7hc/ui/test/unit/controller/purchaseOrderDetail.controller"
+], function () {
+	"use strict";
+});
